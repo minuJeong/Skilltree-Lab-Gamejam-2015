ed757fb7d68c598ad7fccd835e2a7e49e3b62606
minuj <minu.hanwool@gmail.com>
2015-08-08 00:03:56 +0900

    Initial Commit

---
+++ src/Assets/Scripts/ScreenSetup.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScreenSetup : MonoBehaviour
+{
+	// Use this for initialization
+	void Start ()
+	{
+		Screen.SetResolution (1920, 1080, true);
+	}
+}
