7b1b58a5b212e4507ef6176a35a1613619b554e6
minuj <minu.hanwool@gmail.com>
2015-08-08 01:31:15 +0900

    Add NGUI Add Photon Server Add ShaderForge Add Simplygon Add EnvBase

---
+++ src/Assets/Scripts/utility/ApplicationControl.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ApplicationControl : MonoBehaviour
+{
+	// Use this for initialization
+	void Start () {
+		Application.LoadLevelAdditive("1.Home");
+		Application.LoadLevelAdditive("2.HUD");
+	}
+}
